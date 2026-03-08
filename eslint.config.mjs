b3cbe7135e9c68cdf2fe346a68adcdfed6255dd5
awasthinayan <nayanawasthi2015@gmail.com>
2026-03-08 15:31:24 +0530

    added code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'eslint/config';
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
+    plugins: { js, node: true },
     extends: ['js/recommended'],
     languageOptions: { globals: globals.node }
   }
