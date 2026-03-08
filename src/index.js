5f1dc4caf2df93c690ebdcc76ea23e0a8317a711
awasthinayan <nayanawasthi2015@gmail.com>
2026-03-08 23:30:57 +0530

    Update CORS for Vercel frontend

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173',
+       'https://shaadi-biodata-maker-frontend.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
 }));
